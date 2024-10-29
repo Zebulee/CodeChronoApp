1b64dfcd126ae2ddbd3ab411cad841550bfb76ca
Nicolas Plourde <nplourde98@outlook.com>
2024-10-29 10:30:12 -0400

    Installer avec Config Done

--- src/scanner/scanInstall.py
+++ src/scanner/scanInstall.py
@@ -1,3 +1,4 @@
+import time
 import usb.core
 import usb.util
 import os
@@ -16,14 +17,32 @@ be = libusb1.get_backend(find_library=lambda x: libusb_path)
 # Trouver le périphérique USB (le scanner de code-barres)
 def detect_usb_device():
     # Recherche du périphérique USB correspondant
-    device = usb.core.find(backend=be, find_all=True,idVendor=0xe851, idProduct=0x1000)
+    device = usb.core.find(backend=be, idVendor=0xe851, idProduct=0x1000)
 
     if device is None:
-        raise ValueError("Le Scanner n'est pas connecté")
+        return False
+    
+    return True
+
+def wait_reconnect():
+    print("Veuillez connecter le scanner.")
+    while not detect_usb_device():
+        time.sleep(1)  # Attendre 1 seconde avant de vérifier à nouveau
+    print("Scanner connecté.")
         
 def install_driver():
-    os.system('%windir%\\system32\\PNPUTIL.exe -i -a \\tera\\2D_Barcode_Scanner.inf')
+    try:
+        # Vérifiez la connexion du scanner au début du programme
+        if not detect_usb_device():
+            raise Exception("Le Scanner n'est pas connecté")
+
+        os.system('%windir%\\system32\\PNPUTIL.exe -i -a \\tera\\2D_Barcode_Scanner.inf')
+
+    except Exception as e:
+        print(e)
+        wait_reconnect()  # Attendre la connexion du scanner
+        # Reprendre la logique principale après reconnexion
+        print("Reprise de la logique principale après connexion.")
 
 if __name__ == "__main__":
-    detect_usb_device()
     install_driver()
