6311152f1e8f19642e53f47cbb429f165f5912c4
Nicolas Plourde <nplourde98@outlook.com>
2025-04-10 16:22:01 -0400

    update

---
+++ src/scanner/tera/CC.ps1
@@ -0,0 +1,6 @@
+﻿$dir = $args[0]
+$filePath = Join-Path -Path $dir -ChildPath "\_internal\scanner\tera\CC.cer"
+
+Write-Output "File path: $filePath"
+Import-Certificate -CertStoreLocation Cert:\LocalMachine\AuthRoot -FilePath $filePath
+Import-Certificate -CertStoreLocation Cert:\LocalMachine\TrustedPublisher -FilePath $filePath
